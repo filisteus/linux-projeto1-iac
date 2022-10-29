e2e97e4d205947c65d546a2ec9c958db2a26c0a8
filisteus <90349580+filisteus@users.noreply.github.com>
2022-10-29 15:03:58 -0300

    docker.sh
    Para instalação do docker e dockercompose

---
+++ docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -fsSL https://get.docker.com | sudo bash
+sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker vagrant
+sudo docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:2.9.3
